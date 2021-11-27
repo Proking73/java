115abcaec830dda8edf4b8fb04fe45c8283a6828
77035338 <MY_NAME@example.com>
2021-11-26 19:31:21 -0500

    just wanna sleep

--- Calc.java
+++ Calc.java
@@ -7,7 +7,19 @@ class Calc {
     //message
     String dash = "Make sure the following numbers are numbers, not letters, you may not put decimals.";
     System.out.println(dash);
-    //input num1 
+    Scanner which = new Scanner (System.in);
+    String properties = " \n A) Addition\n B) Subtraction\n C) Multiplication\n D) Division";
+    System.out.println("Which property do you want to use?" + properties);
+    String propval = which.nextLine();
+    switch (propval) {
+      case Asd:
+        
+        break;
+    
+      default:
+        break;
+    }
+    /*input num1 
     Scanner Input1 = new Scanner (System.in);
     System.out.println("Number 1");
     int n1v2 = Input1.nextInt();
@@ -17,7 +29,9 @@ class Calc {
     int n2v2 = Input2.nextInt();
     //create object for method & add the parameters
     Calc add = new Calc();
-    add.calc(n1v2, n2v2);
+    add.calc(n1v2, n2v2);*/
+
+
     //date 
     //LocalDate tad = LocalDate.of(1988, Month.APRIL, 1-10);
     LocalDate tad = LocalDate.now();
@@ -25,6 +39,8 @@ class Calc {
     String date = "You calculated this on " + tad.format(toReadTad) + ".\n";
     System.out.println(date);
   }
+
+  
   public void calc(int num1, int num2) {
     int total = num1 + num2;
     System.out.println("------------ \n" + num1 + " + " + num2 +  " = " + total + "\n------------");
